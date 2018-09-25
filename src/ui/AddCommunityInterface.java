589294ed4a3cc89e990d1060fd19732f19c7b57f
Melissa B <melissabernstein.ubc@gmail.com>
2018-09-25 10:52:33 -0700

    Added basic GUI interface of adding a community incomplete refactoring of addnewcommunity interface - make a new community - add it to list of communities (list in community map) - different interfaces for different uses -- intro screen -- adding new community -- viewing list of community - make it testable (pass in parameters to methods) Todo - understand how JavaFX works: window, scene, etc.

---
+++ src/ui/AddCommunityInterface.java
@@ -0,0 +1,74 @@
+package ui;
+
+import model.*;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AddCommunityInterface {
+
+    Community newCommunity;
+
+    public AddCommunityInterface(Stage window){
+
+
+        //GridPane with 10px padding around edge
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        //Name Label - constrains use (child, column, row)
+        Label nameLabel = new Label("Name:");
+        GridPane.setConstraints(nameLabel, 0, 0);
+
+        //Name Input
+        TextField nameInput = new TextField("");
+        GridPane.setConstraints(nameInput, 1, 0);
+
+        //Population Label
+        Label populationLabel = new Label("Population:");
+        GridPane.setConstraints(populationLabel, 0, 1);
+
+        //Population Input
+        TextField populationInput = new TextField();
+        populationInput.setPromptText("");
+        GridPane.setConstraints(populationInput, 1, 1);
+
+        //Zipcode Label
+        Label zipcodeLabel = new Label("Zipcode:");
+        GridPane.setConstraints(zipcodeLabel, 0, 2);
+
+        //Zipcode Input
+        TextField zipcodeInput = new TextField();
+        zipcodeInput.setPromptText("");
+        GridPane.setConstraints(zipcodeInput, 1, 2);
+
+        //Language Label
+        Label languageLabel = new Label("Language:");
+        GridPane.setConstraints(languageLabel, 0, 3);
+
+        //Language Input
+        TextField languageInput = new TextField();
+        languageInput.setPromptText("");
+        GridPane.setConstraints(languageInput, 1, 3);
+
+        //Submit Button
+        Button loginButton = new Button("Submit");
+        GridPane.setConstraints(loginButton, 1, 4);
+        // loginButton.setOnAction( e -> new CommunityMap());
+
+
+        //Add everything to grid
+        grid.getChildren().addAll(nameLabel, nameInput, populationLabel, populationInput, zipcodeLabel, zipcodeInput, languageLabel, languageInput, loginButton);
+
+        Scene scene = new Scene(grid, 300, 200);
+        scene.getStylesheets().add("StyleSheet.css");
+        window.setScene(scene);
+    }
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,15 +1,40 @@
 package ui;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import model.*;
 
-//import Java Fx - plans to make a GUI (or use Swing and use JFrame...)
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
-public class Main {
+public class Main extends Application {
+    Stage window;
+
     public static void main(String[] args) {
 
-        CommunityMap map1 = new CommunityMap();
+      launch(args);
+      CommunityMap map1 = new CommunityMap();
 
-
-        }
     }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("Community Map");
+
+        AddCommunityInterface start = new AddCommunityInterface(window);
+
+        window.show();
+    }
+
+}
+
+//source: https://github.com/buckyroberts/Source-Code-from-Tutorials/blob/master/JavaFX/000_template/Main.java
+
